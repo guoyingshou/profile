a6f11f80203e0e6a3776733d132f4c6ef7ca43c6
guoyingshou <guoyingshou@tianji.com>
2013-01-09 13:33:41 +0800

    merged project

--- profile-dao/src/main/java/com/tissue/profile/dao/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.tissue.profile.dao;
-
-public class DuplicateEmailException extends Exception {
-    public DuplicateEmailException(String message, Throwable e) {
-        super(message, e);
-    }
-}
